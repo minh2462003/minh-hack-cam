06235f96d9a2d8cb19f8a371d29768f594e82cfe
minh2462003 <168170486+minh2462003@users.noreply.github.com>
2024-05-30 10:30:07 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,9 @@ ip_input = st.text_input("IP Address to brute force")
 a = st.text_input("1st num")
 b = st.text_input("2nd num")
 
-if(a != None and b != None):
-    result = testSum(a, b)
-    st.write("Target IP address is", result)
+if a and b:  # Check if both inputs are not empty
+    try:
+        result = testSum(a, b)
+        st.write("Sum of the numbers is", result)
+    except ValueError:
+        st.write("Please enter valid numbers.")
