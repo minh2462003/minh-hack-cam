e16efc6931b6ce288a02feb63038426d8c375f0b
minh2462003 <168170486+minh2462003@users.noreply.github.com>
2024-05-30 10:28:42 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,6 @@ ip_input = st.text_input("IP Address to brute force")
 a = st.text_input("1st num")
 b = st.text_input("2nd num")
 
-result = testSum(a, b)
-
-st.write("Target IP address is", result)
+if(a != None && b != None):
+    result = testSum(a, b)
+    st.write("Target IP address is", result)
